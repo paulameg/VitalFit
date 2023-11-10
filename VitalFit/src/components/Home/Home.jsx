5c29e2ebbdfa7ae367397f8457e3de169538a4da
paulameg <paulatakahashi4@gmail.com>
2023-11-09 22:22:02 -0300

    Home page

---
+++ VitalFit/src/components/Home/Home.jsx
@@ -0,0 +1,18 @@
+import './Home.css'
+
+const Home = () => {
+  return (
+    
+    <div className="home">
+        <div className='cta'>
+            <h1 className='title'>Transforme sua vida com a VitalFit</h1>
+            <p className='text'>Seu caminho para uma vida mais saudável e ativo.</p>
+            <button className='btn'>Agende sua visita grátis</button>
+        </div>
+        
+    </div>
+
+  )
+}
+
+export default Home
