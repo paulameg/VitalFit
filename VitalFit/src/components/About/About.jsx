958baed6db23cc5fb6ba8487ff338b63f3102373
paulameg <paulatakahashi4@gmail.com>
2023-11-10 12:16:02 -0300

    About section

---
+++ VitalFit/src/components/About/About.jsx
@@ -0,0 +1,16 @@
+import './About.css'
+
+const About = () => {
+  return (
+    <div className='about'>
+        <section className='about-box'>
+            <h1 className='about-title'>VitalFit</h1>
+            <p className='about-text'>
+                Bem-vindo à VitalFit! Somos uma academia comprometida em promover um estilo de vida saudável e equilibrado. Nossa equipe qualificada e instalações modernas oferecem uma variedade de atividades, incluindo musculação, aulas de Muay Thai e FitDance. Estamos aqui para ajudar você a alcançar seus objetivos de fitness e bem-estar. Junte-se a nós na VitalFit e transforme sua vida hoje!
+            </p>
+        </section>
+    </div>
+  )
+}
+
+export default About
